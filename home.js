89400a86064bd517960d69da9217043d45aa3c46
paulzpranay97 <paulpranay1997@gmail.com>
2023-02-26 21:12:01 +0530

    done with home page navbar fotter boday sign in sign up

--- home.js
+++ home.js
@@ -67,6 +67,10 @@ window.addEventListener("load", function () {
        $('.signup, .signin, #pop').hide();
        
      }
+     else if(emailin.value==="xyz@admin.com" && passwordin.value==="admin@1234"){
+      alert("Redirecting to Admin Page")
+      window.location.href= "home.html"
+     }
      else{
      
        alert("Wrong Credentials")
@@ -76,3 +80,188 @@ window.addEventListener("load", function () {
    }
 
  )
+//////////////////////////////////////////////////////////////////
+// sliding section 4 parts 
+//  sliding js   
+// common
+let left=1;
+    let right=4;
+    let left1=1;
+    let right1=4;
+    let left2=1;
+    let right2=4;
+    let left3=1;
+    let right3=4;
+    
+    function show(){
+    for (let i=left;i<=right;i++)
+      {
+        document.getElementById("c"+i).style.display="inline-block";
+      }
+    for (let i=left1;i<=right1;i++)
+      {
+        document.getElementById("d"+i).style.display="inline-block";
+        
+      }
+      for (let i=left2;i<=right2;i++)
+      {
+        document.getElementById("e"+i).style.display="inline-block";
+        
+      }
+      for (let i=left3;i<=right3;i++)
+      {
+        document.getElementById("f"+i).style.display="inline-block";
+        
+      }
+      
+    }
+
+// cccccccccccccccccccccccccccccccccccccccccccccccccccccc
+    // unique Selection
+    // c
+        function moveRight(){
+      if(left<=4 && right<=7)
+      {
+        document.getElementById("c"+left).style.display="none";
+        left+=1;
+        right+=1;
+        for (let i=left;i<=right;i++)
+        {
+          document.getElementById("c"+i).style.display="inline-block";
+        }
+      }
+      else{
+        return;
+      }
+      
+    }
+
+    function moveLeft()
+    {
+      if(left>=2 && right>=5)
+      {
+        document.getElementById("c"+right).style.display="none";
+        left-=1;
+        right-=1;
+        for (let i=left;i<=right;i++)
+        {
+          document.getElementById("c"+i).style.display="inline-block";
+        }
+      }
+      else{
+        return;
+      }
+      
+    }
+    // ddddddddddddddddddddddddddddddddddddddddddddddddddddd
+    // d
+    function moveRight1()
+    {
+      if(left1<=4 && right1<=7)
+      {
+        document.getElementById("d"+left1).style.display="none";
+        left1+=1;
+        right1+=1;
+        for (let i=left1;i<=right1;i++)
+        {
+          document.getElementById("d"+i).style.display="inline-block";
+        }
+      }
+      else{
+        return;
+      }
+      
+    }
+
+    function moveLeft1()
+    {
+      if(left1>=2 && right1>=5)
+      {
+        document.getElementById("d"+right1).style.display="none";
+        left1-=1;
+        right1-=1;
+        for (let i=left1;i<=right1;i++)
+        {
+          document.getElementById("d"+i).style.display="inline-block";
+        }
+      }
+      else{
+        return;
+      }
+      
+    }
+// eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+// e
+    function moveRight2()
+    {
+      if(left2<=4 && right2<=7)
+      {
+        document.getElementById("e"+left2).style.display="none";
+        left2+=1;
+        right2+=1;
+        for (let i=left2;i<=right2;i++)
+        {
+          document.getElementById("e"+i).style.display="inline-block";
+        }
+      }
+      else{
+        return;
+      }
+      
+    }
+
+    function moveLeft2()
+    {
+      if(left2>=2 && right2>=5)
+      {
+        document.getElementById("e"+right2).style.display="none";
+        left2-=1;
+        right2-=1;
+        for (let i=left2;i<=right2;i++)
+        {
+          document.getElementById("e"+i).style.display="inline-block";
+        }
+      }
+      else{
+        return;
+      }
+      
+    }
+    // ffffffffffffffffffffffffffffffffffffffffffffffffffff
+    // f
+    function moveRight3()
+    {
+      if(left3<=4 && right3<=7)
+      {
+        document.getElementById("f"+left3).style.display="none";
+        left3+=1;
+        right3+=1;
+        for (let i=left3;i<=right3;i++)
+        {
+          document.getElementById("f"+i).style.display="inline-block";
+        }
+      }
+      else{
+        return;
+      }
+      
+    }
+
+    function moveLeft3()
+    {
+      if(left3>=2 && right3>=5)
+      {
+        document.getElementById("f"+right3).style.display="none";
+        left3-=1;
+        right3-=1;
+        for (let i=left3;i<=right3;i++)
+        {
+          document.getElementById("f"+i).style.display="inline-block";
+        }
+      }
+      else{
+        return;
+      }
+      
+    }
+    /////////////////////////////////////////////////////////
